7d481dfe4d431c2cdb8cf781a1e310d25f91f5cb
Bartek Janiszewski <bjnisze@gmail.com>
2019-04-05 12:48:06 +0200

    Initial commit

---
+++ src/main/java/pl/bartekjaniszewski/springbootcourse/User.java
@@ -0,0 +1,16 @@
+package pl.bartekjaniszewski.springbootcourse;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean author;
+    private boolean admin;
+
+
+}
